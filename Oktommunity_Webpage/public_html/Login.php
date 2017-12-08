9b2469d10c4a7e2d77697e69dde112a7061dc5a0
owrior <o.w.riordan@gmail.com>
2017-12-08 09:47:28 +0000

    Changing launch controls

--- Oktommunity_Webpage/public_html/Login.php
+++ Oktommunity_Webpage/public_html/Login.php
@@ -1,14 +1,29 @@
 <?php
 
-connect.connect();
-$login_query = "SELECT Password FROM Customers WHERE Email LIKE "+$email_entered;
+    connect.connect(true);
 
-$password = query($connection,$login_query);
-if ($password == $password_entered) {
-    return 1;
-}
-else {
-    return 0;
-}
+    function getUser(){
+        $user = array();
+        $user['email']= $_POST['email_entered'];
+        $user['password']=$_POST['password_entered'];
+        return $user;
 
+    }
 
+    $user = getUser();
+
+    $login_query = "SELECT Password FROM Customers WHERE Email LIKE '$email_1';";
+    
+    $password = query($connection,$login_query);
+    
+    if ($password == $password_entered) {
+        echo "worked";
+        connect.connect(false);
+        header ('Location: index.html');
+    }
+    else {
+        echo "fucked";
+        connect.connect(false);
+        header('Location: index.html');
+    }
+?>
--- Oktommunity_Webpage/public_html/connect.php
+++ Oktommunity_Webpage/public_html/connect.php
@@ -1,12 +1,17 @@
 <?php 
-    function connect(){
-        $dbhost = 'localhost';
-        $dbuser = 'username';
-        $dbpass = 'password';
-        $dbname = 'Oktober';
-        $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die('Error connecting to MySQL server.'.mysql_error());
-        return $connection;
+        function connect($type=bool){
+            if ($type == true) {
+                $dbhost = 'localhost';
+                $dbuser = 'root';
+                $dbpass = 'root';
+                $dbname = 'Oktober';
+                $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die('Error connecting to MySQL server.'.mysql_error());
+                return $connection;
+            }
+            else {
+                mysqli_close($connection);
+            }
         }
-        
+?>        
 
 
