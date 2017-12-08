da64fdf7689e682e0ae7f0d5da49c4f5d5985694
aimanesham <aimanesham@eduroam-int-dhcp-97-231-49.ucl.ac.uk>
2017-12-08 14:08:34 +0000

    oio

--- Oktommunity_Webpage/public_html/index.php
+++ Oktommunity_Webpage/public_html/index.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel='stylesheet' href='stylesheet.css'>
     </head>
-    <body>
+    <body style="background-image:url(Home.jpg)">
         <div class='login_control_dropdown'>
             <button class="login_control_btn">User</button>
             <div class = 'login_control_content'>
@@ -31,6 +31,7 @@
         <p>Hello there, we here at Oktommunity are a platform on which you may either buy tickets to attend events or host your own! This is mainly for 
         Oktoberfest so then will be the hottest time to attend events however there are beer festivals all year round so come along fill up a stein and 
         enjoy!!!</p>
+      
         </div>
         <div class='sidebar'>
         <h2 class="sidebar">WHAT'S<br/> ON?</h2>
