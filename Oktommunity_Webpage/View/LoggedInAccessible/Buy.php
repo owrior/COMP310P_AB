7cc0168a3bdd286d911a4c381eb261c0589d54bf
aimanesham <aimanesham@eduroam-int-dhcp-97-231-49.ucl.ac.uk>
2017-12-10 15:12:48 +0000

    buy page creation

---
+++ Oktommunity_Webpage/View/LoggedInAccessible/Buy.php
@@ -0,0 +1,34 @@
+!DOCTYPE html>
+<?php session_start() ?>
+<html>
+    <head>
+        <title>Event's</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel='stylesheet' href='/View/stylesheet.css'>
+    </head>
+    <body>
+        <div class='login_control_dropdown'>
+            <button class="login_control_btn"><?php 
+            if ($_SESSION['email'] == NULL) {
+                echo 'User';
+            }
+            else {
+                echo $_SESSION['email'];
+            }
+            ?></button>
+            <div class = 'login_control_content'>
+                <a href='/Controller/LogOut.php'>Log Out</a>
+            </div>
+        </div>
+        <br>
+        <div class='navigation_control'>
+            <ul class='navigation_control'>
+                <li class='navigation_control'><a href=LoggedInHomepage.php>OKTOMUNITY</a></li>
+                <li class='navigation_control'><a href=LoggedInEvents.php>Events</a></li>
+                <li class='navigation_control'><a href=Tokens.php>Tokens</a></li>
+                <li class='navigation_control'><a href=Create.php>Create</a></li>
+                <li class='navigation_control'><a href=Control.php>Control</a></li>
+            </ul>
+        </div>
+        <div action='/Controller/Event_Info_Retrieval.php'
