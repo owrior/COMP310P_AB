7bd017d0c289daf1651093c2406fcbe6d5d7cbec
owrior <o.w.riordan@gmail.com>
2017-12-14 17:32:34 +0000

    Buying stuff changes

--- Oktommunity_Webpage/View/LoggedInAccessible/Control.php
+++ Oktommunity_Webpage/View/LoggedInAccessible/Control.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <?php 
-    require '/Controller/connect.php';
     session_start();
 ?>
 <html>
@@ -47,31 +46,32 @@
             </tr>
             <?php 
             if(1==1){
+                require '/Controller/connect.php';
                 $connection = connect();
                 $OwnerID = $_SESSION['loginID'];
-             $event_query = "SELECT * FROM event WHERE OwnerID = '$OwnerID'";
-        $result = mysqli_query($connection,$event_query);
-        while($row = mysqli_fetch_assoc($result)){
-            
-            $name = $row[Event_Name];
-            $Date = $row[Event_Date];
-            $Location = $row[Location_ID];
-            $location_query = "SELECT * FROM location WHERE Location_ID ='$Location' ";
-            $result2 = mysqli_query($connection,$location_query);
-            $row2 = mysqli_fetch_assoc($result2);
-            $locationName = $row2['Name'];
-            echo '<tr>';
-            echo '<td>'.$name.'</td>';
-            echo '<td>'.$Date.'</td>';
-            echo '<td>'.$locationName.'</td>';
-            echo'</tr>';
-            }}
-            ?>
-            </table>
-                
-            
-                
-        </div>
+                $event_query = "SELECT * FROM event WHERE OwnerID = '$OwnerID'";
+                $result = mysqli_query($connection,$event_query);
+                while($row = mysqli_fetch_assoc($result)){
+
+                    $name = $row[Event_Name];
+                    $Date = $row[Event_Date];
+                    $Location = $row[Location_ID];
+                    $location_query = "SELECT * FROM location WHERE Location_ID ='$Location' ";
+                    $result2 = mysqli_query($connection,$location_query);
+                    $row2 = mysqli_fetch_assoc($result2);
+                    $locationName = $row2['Name'];
+                    echo '<tr>';
+                    echo '<td>'.$name.'</td>';
+                    echo '<td>'.$Date.'</td>';
+                    echo '<td>'.$locationName.'</td>';
+                    echo'</tr>';
+                    }}
+                    ?>
+                    </table>
+
+
+
+                </div>
         
         <div class ="eventHosted">
             <h>EVENTS YOU ARE </h>
