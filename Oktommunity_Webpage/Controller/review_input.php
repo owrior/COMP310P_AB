f716149f636902a01920fc0b834c234b6c0de008
owrior <o.w.riordan@gmail.com>
2017-12-15 15:32:47 +0000

    Review Stuff

--- Oktommunity_Webpage/Controller/review_input.php
+++ Oktommunity_Webpage/Controller/review_input.php
@@ -1,7 +1,22 @@
 <?php
 ini_set('display',1);
+session_start();
 include 'connect.php';
 
-$event_ID = $_POST['event_ID'];
-$review_score = $_POST[''];
+function getUserID($email) {
+    $connection = connect();
+    $customer_ID_query = "SELECT Customer_ID FROM Customer WHERE Email ='$email'";
+    $result = mysqli_query($connection, $customer_ID_query);
+    $customer_ID_Array = mysqli_fetch_array($result);
+    $customer_ID = $customer_ID_Array[0];
+    disconnect($connection);
+    return $customer_ID;
+}
 
+$event_ID = $_POST['event_ID'];
+$review_score = $_POST['review_score'];
+$customer_ID =getUserID($_SESSION['email']);
+
+$review_insert_query = "INSERT INTO rating (Customer_ID, Event_ID, Rating) VALUES ('$customer_ID', '$event_ID', '$review_score')";
+mysqli_query($connection,$review_insert_query);
+header('location: ../View/LoggedInView/LoggedInEvents.php');
